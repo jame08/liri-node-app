92f3296baa82f7def3b5c0fa67949e81b108fd99
Jonas M <jonasmorel8@gmail.com>
2018-08-23 19:52:52 -0400

    Spotify Promise

--- liri.js
+++ liri.js
@@ -1,27 +1,43 @@
 
 
-var request = require("request");
-var keys = require ("./keys.js");
 require("dotenv").config();
+var keys = require ("./keys.js");
+var request = require("request");
+var fs = require('fs');
+var logs = require('log-timestamp');
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 
-//OMDB Request
 
-var movie = inputString.slice(1); //variable that takes all the argv elements but positions 0 and 1.
-var inputString = process.argv.slice(2); //variable that takes all the argv elements but positions 0, 1,2
+var divider ="\n------------------------------------------------------------\n\n";
 
-if (inputString[0] === "movie-this"){
+//User Input
+var cliInput = process.argv.slice(2);
+var inputString =cliInput.slice(1).join(" "); //variable that takes all the argv elements but positions 0, 1
+var term = cliInput[0] //variable that takes all the argv elements but positions 0.
+//User Input ^
+
+//App Logic
+if (term === "movie-this"){
 
 //if the the array movie has no element the if will excecute.
-if(!movie.length){
-movie= "Mr. Nobody";
-omdbRequest(movie);
+if(!inputString.length){
+
+omdbRequest("Mr. Nobody");
 }
 else {
 
-    omdbRequest(movie);
+omdbRequest(inputString);
 }
 
+} else if (term === "spotify-this-song"){
+
+spotifyRequest(inputString);
 }
+//App Logic ^
+
+
+
 
 // function for the OMDB request.
 function omdbRequest(movie){
@@ -30,19 +46,57 @@ request("http://www.omdbapi.com/?t="+movie+"&plot=short&apikey=trilogy", functio
    
   if (!error && response.statusCode === 200) {
     
-    console.log("***************************************************");
-    console.log("Title: " + JSON.parse(body).Title);
-    console.log("Year: " + JSON.parse(body).Year);
-    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-    console.log("Country: " + JSON.parse(body).Country);
-    console.log("Language: " + JSON.parse(body).Language);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Cast: " + JSON.parse(body).Actors);
-    console.log("***************************************************");
+    var showData = [
+      "Title: " + JSON.parse(body).Title,
+      "Year: " + JSON.parse(body).Year,
+      "IMDB Ranting: " + JSON.parse(body).imdbRating,
+      "Country: " + JSON.parse(body).Country,
+      "Language: " + JSON.parse(body).Language,
+      "Plot: " + JSON.parse(body).Plot,
+      "Cast: " + JSON.parse(body).Actors
+    ].join("\n\n");
+   saveLogs(showData,'OMDB',movie);
+    console.log(divider + showData + divider)
   }
 
   
 });
 }
 
-// OMDB Request end.
+
+
+// OMDB Request ^
+
+
+//Spotify Request.
+
+function spotifyRequest(userString) 
+{
+spotify.search({ type: 'track', query: userString })
+.then(function(response) {
+  console.log(response);
+})
+.catch(function(err) {
+  console.log(err);
+});
+
+}
+
+
+// Spotify request ^
+
+function saveLogs(showData,apiname,search){
+
+  function timestamp() {
+    return 'API Called to ' + apiname + ' was made at ' + '[' + new Date().toISOString() + ']' + ' searching for ' + search;
+  }
+
+  var time = timestamp();
+
+  fs.appendFile("logs.txt", divider + time + divider, function(err) {
+    if (err) throw err;
+
+    console.log(showData);
+  });
+
+}
